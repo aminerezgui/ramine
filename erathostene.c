bd7cfebd6b98d0ac5e3bae6ce42ff167e8fa8cae
amine <armean@hotmail.fr>
2016-09-15 13:18:04 +0200

    maths

---
+++ erathostene.c
@@ -0,0 +1,39 @@
+#include <unistd.h>
+#include <stdarg.h>
+
+void erathostene(int a)
+{
+	int k;
+	int i;
+	int *tab;
+	tab = (int*)malloc(sizeof(int) * a - 1);
+
+	i = 0;
+	k = 2;
+	while (i < a - 1)
+	{
+		tab[i] = k;
+		i++;
+		k++;
+	}
+	i = 0;
+	while (i < a - 1)
+	{
+		k = i + 1;
+		while (k < a - 1)
+		{
+			if (tab[k] % tab[i] == 0)
+				tab[k] = 0;
+			k++;	
+		}
+		i++;
+	}
+	i = 0;
+	k = 1;
+	while (i < a - 1)
+	{
+		if (tab[i] != 0)
+			ft_printf("%d est le n°%d nombre  premier\n", tab[i], k++);
+		i++;
+	}
+}
---
+++ ft_gotoprintf.c
@@ -0,0 +1,71 @@
+#include <unistd.h>
+#include <stdarg.h>
+
+void ft_putstr(char *str)
+{
+	int i;
+	i = 0;
+
+	while (str[i])
+	{
+		write(1, str + i, 1);
+		i++;
+	}
+}
+
+int ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return (0);
+}
+
+void ft_putnbr(int nbr)
+{
+	if (nbr == -2147483648)
+	{
+		ft_putstr("-2147483648");
+		return ;
+	}
+	if (nbr < 0)
+	{
+		nbr = -nbr;
+		ft_putchar('-');
+		
+	}
+	if (nbr >= 0 && nbr <= 9)
+		ft_putchar(nbr + 48);
+	else
+	{
+		ft_putnbr(nbr / 10);
+		ft_putnbr(nbr % 10);
+	}
+}
+
+void ft_printf(char *str, ...)
+{
+	int i;
+	char *next;
+	int next_int;
+	va_list ap;
+
+	va_start(ap, str);
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '%' && str[i + 1] == 's')
+		{
+			next = va_arg(ap, char*);
+			ft_putstr(next);
+			i+=2;
+		}
+		if (str[i] == '%' && str[i + 1] == 'd')
+		{
+			next_int = va_arg(ap, int);
+			ft_putnbr(next_int);
+			i+=2;
+		}
+		write(1, str + i, 1);
+		i++;
+	}
+	va_end(ap);
+}
